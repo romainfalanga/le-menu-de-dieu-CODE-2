cb821a817377fa2f428ecd7f4d3f7d9c27273d90
romainfalanga <61136596+romainfalanga@users.noreply.github.com>
2025-09-22 11:25:50 +0200

    Updated UniverseAppPage.tsx

--- src/pages/UniverseAppPage.tsx
+++ src/pages/UniverseAppPage.tsx
@@ -515,7 +515,7 @@ De même, le JavaScript qui compose un composant React n'existe pas à l'avance 
                       {[
                         {
                           level: "Composants React",
-                          desc: "Composants réutilisables : boutons, formulaires",
+                          desc: "Composants de la réalité réutilisables",
                           connection: "→ Logique et structure définies par le code JavaScript",
                           color: "bg-green-900/40 border-green-400"
                         },
